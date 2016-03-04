e8368d638b3da425b93fa57ac1671970a7a2e816
jobryan205 <jobryan205@gmail.com>
2016-03-04 10:58:42 -0500

    restructuring stylesheets

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
     $(".navbar").hide(0).delay(1300).fadeIn(1000);
+    $("#arrow").hide(0).delay(1300).fadeIn(1000);
+    $("#arrow").mouseenter(function() {
+        $(this).effect("bounce", 1000);
+    });
     $("#sugar-logo").hide(0).fadeIn(2500);
     console.log("confirmation");
 });
